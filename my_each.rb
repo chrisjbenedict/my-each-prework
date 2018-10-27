8a74118bd2edad0e4387cf8623c9c002c21c5987
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-27 15:37:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  i = 0
+  while i < arr.size
+    arr[i]
+    yield(arr[i])
+    i += 1
+  end
+  return arr
 end
